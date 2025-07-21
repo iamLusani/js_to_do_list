58e13575c2577b4350368444043d1bf043f92206
iamLusani <lusani.22mphigalale@gmail.com>
2025-07-21 20:45:20 +0200

    tasklist added
    user can write tasks and javascript will show the task you written in html

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 const name = prompt("What is your name?");
 let heading = document.querySelector("h1");
 heading.textContent = `Hiii, ${name} ready to write some tasks!`;
+
+let form = document.querySelector("form");
+let input = document.querySelector("#inputText");
+let userInput = document.querySelector("#taskList");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const taskText = input.value.trim();
+  if (taskText !== "") {
+    const li = document.createElement("li");
+    li.className = "todo-item";
+    li.innerHTML = `
+    <span>${taskText}</span>
+    
+  `;
+    userInput.appendChild(li);
+    input.value = "";
+  }
+});
